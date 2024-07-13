372c377ea379d5bfa41f823a3783c615aca70c76
Philipp Defner <mail@notmyhostna.me>
2024-07-13 22:37:15 +0200

    Fix

--- src/triggers/nginx-app-template-source.go
+++ src/triggers/nginx-app-template-source.go
@@ -11,6 +11,10 @@ func main() {
 		fmt.Println("Usage: nginx-app-template-source <arg1> <arg2>")
 		return
 	}
+	// We only want to use this custom template for the app-config argument (https://github.com/dokku/dokku/discussions/6991#discussioncomment-10040689)
+	if args[2] != "app-config" {
+		return
+	}
 	appPluginConfigPath := fmt.Sprintf("/var/lib/dokku/data/nginx-override-by-app/%s/nginx.conf.sigil", args[1])
 	if _, err := os.Stat(appPluginConfigPath); err != nil {
 		if os.IsNotExist(err) {
